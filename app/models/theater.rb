e0e11ee55a9ef9df51444733ed8044f53194eb68
Puttaraj Koliwad <puttarajkoliwad@gmail.com>
2020-11-04 15:17:55 +0530

    Time slots to book ticket added

--- app/models/theater.rb
+++ app/models/theater.rb
@@ -5,21 +5,25 @@ class Theater < ApplicationRecord
   has_many :users, through: :bookings
   attr_accessor :rows
 
-  def self.show_timings
-    {"09:00 AM" => seats, "12:00 PM" => seats, "03:00PM" => seats, "06:00 PM" => seats, "09:00 PM"=> seats}
+  def timings
+    ["09:00 AM", "12:00 PM", "03:00PM", "06:00 PM", "09:00 PM"]
   end
 
-  def self.seat_layout
+  def seat_layout
     @rows = ("A".."R").to_a
     cols = (1..22).to_a
     cols.fill(false)
-    @seats = Array.new(18)
-    @seats.fill(cols)
+    seats = Array.new(18)
+    seats.fill(cols)
   end
 
-  def self.availability
+  def show_timings
+    {"09:00 AM" => seat_layout, "12:00 PM" => seat_layout, "03:00PM" => seat_layout, "06:00 PM" => seat_layout, "09:00 PM"=> seat_layout}
+  end
+
+  def availability
     show_timings.each do |slot|
-      slot = {slot => [seats]}
+      slot = {slot => [seat_layout]}
     end
   end
 
