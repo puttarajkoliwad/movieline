9dd131c4b64287df5730150647fd28f84d72c7bb
Puttaraj Koliwad <puttarajkoliwad@gmail.com>
2020-11-04 15:29:04 +0530

    Booking through time slots and wallet balance

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   get 'book_ticket', to: 'theaters#book'
+  #post 'select_seats', to: theaters#select_seats
   
 end
+#selva sir 9791184989
