832fcb75ce3fde9f89a5db2310b6e65fcd418b2c
Puttaraj Koliwad <puttarajkoliwad@gmail.com>
2020-11-07 04:41:13 +0530

    wallet recharge styled. Working prototype with minimal deployable features

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -4,7 +4,7 @@ class ChargesController < ApplicationController
   
   def create
     # Amount in cents
-    @amount = 1000
+    @amount = 100000
   
     customer = Stripe::Customer.create({
       email: params[:stripeEmail],
